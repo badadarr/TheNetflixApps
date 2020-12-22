f91574fb6aba7cb660fa9619a1c0cefbd5af3666
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-22 20:45:03 +0700

    Oi

---
+++ Shared/The_Netflix_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Netflix_AppsApp.swift
+//  Shared
+//
+//  Created by badar on 22/12/20.
+//
+
+import SwiftUI
+
+@main
+struct The_Netflix_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
